27d198ec7c332e15666c5a5cd839567c8dd52604
FromElisur <njrodriguezp@unah.hn>
2022-11-29 00:03:52 -0600

    arreglo

--- resources/views/falla/index.blade.php
+++ resources/views/falla/index.blade.php
@@ -8,7 +8,6 @@
 
 @section('content')
 
-
 <div class="container-fluid">
     <div class="row content">
         <div class="col-sm-12">
@@ -17,8 +16,18 @@
                     <h3 class="card-title">-</h3>
                 </div>
                 <!-- /.card-header -->
-<<<<<<< HEAD
                 <div class="card-body">
+                    <tr>
+                        <div class="form-group">
+                            <label>Fecha:</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                                </div>
+                                <input type="text" class="form-control  col-md-5" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask="" im-insert="false">
+                            </div>
+                        </div>
+                    </tr>
                     <tr>
                         <label>Datos personales</label>
                         <div class="form-row">
@@ -34,7 +43,6 @@
                         </div>
                     </tr>
                     <tr>
-<<<<<<< HEAD
                         <div class="form-group col-md-6">
                             <label>Servicio:</label>
                             <select id="inputState" class="form-control">
@@ -46,9 +54,6 @@
                     </tr>
                     <tr>
                         <label>Tema:</label>
-=======
-                        <label>Servicios:</label>
->>>>>>> 366dbc8baa39a6154a92242989b6071a6e05ade6
                         <div class="form-group">
                             <div class="form-group col-md-6">
                                 <input type="text" class="form-control" id="inputCity" placeholder="">
@@ -61,93 +66,35 @@
                         <td class="text-right py-0 align-middle">
                             <form>
                                 <div class="form-row align-items-center">
-=======
->>>>>>> 640b0290eda09d052df44e0aced692f05a6b9f88
 
-
-
-
-
-                <div class="card-body">
-
-                    <form action="/falla" method="POST">
-
-                        @csrf
-
-
-
-                        <tr>
-                            <label>Datos personales</label>
-
-
-
-                            <div class="form-row">
-                                <div class="form-group col-md-4">
-                                    <input id="NOMBRE" name="NOMBRE" type="text" class="form-control" placeholder="Nombre">
-                                </div>
-                                <div class="form-group col-md-4">
-                                    <input id="TELEFONO" name="TELEFONO" type="text" class="form-control" placeholder="Telefóno">
-                                </div>
-                                <div class="form-group col-md-4">
-                                    <input id="TCORREO_ELECTRONICO" name="CORREO_ELECTRONICO" type="text" class="form-control" placeholder="Correo Electrónico">
-                                </div>
-                            </div>
-                        </tr>
-
-                        <tr>                           
-
-                            <div class="form-group col-md-5">
-                                    <select id="COD_SERVICIO" name="COD_SERVICIO"class="form-control">
-                                        <option>Seleccionar Tipo de Servicio</option>
-                                        <option value="1">Aíre Acondicionado</option>
-                                        <option value="2">Línea Telefónica</option>
-                                        <option value="3">Sistema de Seguridad</option>
-                                    </select>
-                                </div>
-                                <label>Tema:</label>
-
-                            <div class="form-group">
-                                <div class="form-group col-md-6">
-                                    <input id="TEMA" name="TEMA" type="text" class="form-control" placeholder="tema">
-                                </div>
-                        </tr>
-                        <tr>
-                            <label>Descripción:</label>
-                        </tr>
-
-                        <tr>
-                            <td class="text-right py-0 align-middle">
-                                <form>
-                                    <div class="form-row align-items-center">
-
-                                        <div class="col-auto my-1">
-                                            <!-- textarea -->
-                                            <div class="form-group col-md-20">
-                                                <textarea id="DESCRIPCION" name="DESCRIPCION" rows="2" class="form-control" placeholder="DESCRIPCION"></textarea>
-                                            </div>
+                                    <div class="col-auto my-1">
+                                        <!-- textarea -->
+                                        <div class="form-group">
+                                            <p><textarea class="form-control" rows="4" cols="100" placeholder="enter...."></textarea></p>
                                         </div>
                                     </div>
-                                </form>
-                            </td>
-                        </tr>
-                        <tr>
-                            <label>Ubicación:</label>
-                            <div class="form-group">
-                                <div class="form-group col-md-4">
-                                    <input id="UBICACION" name="UBICACION" type="text" class="form-control" placeholder="">
                                 </div>
-                        </tr>
-                        <tr>
-                            <div class="modal-footer">
-                                <button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
-                            </div>
-                        </tr>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <div class="form-group col-md-6">
+                            <label>Ubicación:</label>
+                            <select id="inputState" class="form-control">
+                                <option>Tegucigalpa</option>
+                                <option>San Pedro Sula</option>
+                                <option>Comayagua</option>
+                            </select>
+                        </div>
+                    </tr>
+                    <tr>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary btn-lg">Enviar</button>
+                        </div>
+                    </tr>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-
-</form>
-@stop
+@endsection
