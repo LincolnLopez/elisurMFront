e84b50545a1edd01a89f68912594c3bb367bc458
LincolnLopez <93690898+LincolnLopez@users.noreply.github.com>
2022-12-02 21:18:17 -0600

    SE cambia menu de cambio de pass

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,4 +1,7 @@
-@extends('layouts.app')
+@extends('adminlte::auth.passwords.email')
+
+@section('title', 'Correo')
+
 
 @section('content')
 <div class="container">
@@ -17,8 +20,8 @@
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -31,7 +34,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
+                        <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Send Password Reset Link') }}
@@ -45,3 +48,4 @@
     </div>
 </div>
 @endsection
+
