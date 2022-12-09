dd5b4ef9948d6b8b73805bcfce837bd9992029a2
angelawaldanc <angela.waldan@unah.hn>
2022-12-09 16:22:55 -0600

    Cambios en el diseño

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
 
     public function adminlte_desc()
     {
-        return "Administrador";
+        return 'Usuario';
     }
 
     public function adminlte_profile_url()
