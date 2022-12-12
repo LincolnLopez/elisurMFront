d41b2c9189d7e936138da320e6a0f9048431f462
ElvinGarcia19 <edgarcial@unah.hn>
2022-12-11 22:38:13 -0600

    poner un nombre al avance

--- resources/views/ticket/edit.blade.php
+++ resources/views/ticket/edit.blade.php
@@ -10,103 +10,144 @@
 
 @section('content')
 <form action="/ticket/{{$ticket->cod_reporte_falla}}" method="POST">
-    @csrf
-    @method('PUT')
+  @csrf
+  @method('PUT')
 
-<!---validació de solo Letras--->
-<script>
-  function soloLetras(e){
-     key = e.keyCode || e.which;
-     tecla = String.fromCharCode(key).toLowerCase();
-     letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
-     especiales = "8-37-39-46";
+  <!---validació de solo Letras--->
+  <script>
+    function soloLetras(e) {
+      key = e.keyCode || e.which;
+      tecla = String.fromCharCode(key).toLowerCase();
+      letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+      especiales = "8-37-39-46";
 
-     tecla_especial = false
-     for(var i in especiales){
-          if(key == especiales[i]){
-              tecla_especial = true;
-              break;
-          }
+      tecla_especial = false
+      for (var i in especiales) {
+        if (key == especiales[i]) {
+          tecla_especial = true;
+          break;
+        }
       }
 
-      if(letras.indexOf(tecla)==-1 && !tecla_especial){
-          return false;
+      if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+        return false;
       }
-  }
-</script>
+    }
+  </script>
+  <script>
+    function DobleEspacio(campo, event) {
 
+      CadenaaReemplazar = "  ";
+      CadenaReemplazo = " ";
+      CadenaTexto = campo.value;
+      CadenaTextoNueva = CadenaTexto.split(CadenaaReemplazar).join(CadenaReemplazo);
+      campo.value = CadenaTextoNueva;
 
-<script>
-function soloNums(e){
-key != e.keyCode || e.which;
-tecla != String.fromCharCode(key).toLowerCase();
-letras != " áéíóúabcdefghijklmnñopqrstuvwxyz";
-especiales != "8-37-39-46";
+    }
+  </script>
 
-tecla_especial != false
-for(var i in especiales){
-if(key == especiales[i]){
-tecla_especial != true;
-break;
-}
-}
+  <script>
+    function soloNums(e) {
+      key != e.keyCode || e.which;
+      tecla != String.fromCharCode(key).toLowerCase();
+      letras != " áéíóúabcdefghijklmnñopqrstuvwxyz";
+      especiales != "8-37-39-46";
 
-if(letras.indexOf(tecla)==-1 && !tecla_especial){
-return false;
-}
-}
-</script>
+      tecla_especial != false
+      for (var i in especiales) {
+        if (key == especiales[i]) {
+          tecla_especial != true;
+          break;
+        }
+      }
 
-
-<script>
-function nospaces1(){
-orig=document.form.PASSWORD_USUARIO.value;
-nuev=orig.split(' ');
-nuev=nuev.join('');
-document.form.PASSWORD_USUARIO.value=nuev;
-if(nuev=orig.split(' ').length>=2);
-}
-</script>
-
-<!---validació de numeros y 2 decimales--->
-<script>
-function limitDecimalPlaces(e, count) {
-if (e.target.value.indexOf('.') == -1) { return; }
-if ((e.target.value.length - e.target.value.indexOf('.')) > count) {
-e.target.value = parseFloat(e.target.value).toFixed(count);
-}
-}
-
-function isNumberKey(evt)
-{
-var charCode = (evt.which) ? evt.which : evt.keyCode;
-if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
-return false;
-
-return true;
-}
-</script>
-
-
-<script>
-
-var validarNumero= function(e) {
-var valor = e.value;
-e.value = (valor.indexOf(".") >= 0) ? (valor.substr(0, valor.indexOf(".")) + valor.substr(valor.indexOf("."), 3)) : valor;
-}
-</script>
+      if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+        return false;
+      }
+    }
 
 
 
-    <div class="mb-3">
+
+
+
+    function sololetras(e) {
+
+      key = e.keyCode || e.wich;
+
+      teclado = String.fromCharCode(key).toUpperCase();
+
+      letras = " ABCDEFGHIJKLMNOPQRSTUVWXYZÑ";
+
+      especiales = "8-37-38-46-164";
+
+      teclado_especial = false;
+
+      for (var i in especiales) {
+
+        if (key == especiales[i]) {
+          teclado_especial = true;
+          break;
+        }
+      }
+
+      if (letras.indexOf(teclado) == -1 && !teclado_especial) {
+        return false;
+      }
+
+    }
+  </script>
+
+
+  <script>
+    function nospaces1() {
+      orig = document.form.PASSWORD_USUARIO.value;
+      nuev = orig.split(' ');
+      nuev = nuev.join('');
+      document.form.PASSWORD_USUARIO.value = nuev;
+      if (nuev = orig.split(' ').length >= 2);
+    }
+  </script>
+
+  <!---validació de numeros y 2 decimales--->
+  <script>
+    function limitDecimalPlaces(e, count) {
+      if (e.target.value.indexOf('.') == -1) {
+        return;
+      }
+      if ((e.target.value.length - e.target.value.indexOf('.')) > count) {
+        e.target.value = parseFloat(e.target.value).toFixed(count);
+      }
+    }
+
+    function isNumberKey(evt) {
+      var charCode = (evt.which) ? evt.which : evt.keyCode;
+      if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+
+      return true;
+    }
+  </script>
+
+
+  <script>
+    var validarNumero = function(e) {
+      var valor = e.value;
+      e.value = (valor.indexOf(".") >= 0) ? (valor.substr(0, valor.indexOf(".")) + valor.substr(valor.indexOf("."), 3)) : valor;
+    }
+  </script>
+
+
+
+  <div class="mb-3">
     <label for="" class="form-label">COD_REPORTE_FALLA</label>
-    <input id="COD_REPORTE_FALLA" name="COD_REPORTE_FALLA" type="text" class="form-control"  value="{{$ticket->cod_reporte_falla}}" readonly>    
+    <input id="COD_REPORTE_FALLA" name="COD_REPORTE_FALLA" type="text" class="form-control" value="{{$ticket->cod_reporte_falla}}" readonly>
   </div>
-    
+
   <div>
     <label for="country">COD_SERVICIO</label>
-    <select class="form-control" id="COD_SERVICIO" name="COD_SERVICIO" value="{{$ticket->cod_servicio}}" >
-      <option value="{{$ticket->cod_servicio}}" >Selecciona nuestros servicios</option>
+    <select class="form-control" id="COD_SERVICIO" name="COD_SERVICIO" value="{{$ticket->cod_servicio}}">
+      <option value="{{$ticket->cod_servicio}}">Selecciona nuestros servicios</option>
       <option value="1">Aire Acondicionado</option>
       <option value="2">Construcción</option>
       <option value="3">Electricidad</option>
@@ -121,40 +162,41 @@ e.value = (valor.indexOf(".") >= 0) ? (valor.substr(0, valor.indexOf(".")) + val
 
   <div class="mb-3">
     <label for="" class="form-label">NOMBRE</label>
-    <input id="NOMBRE" name="NOMBRE" type="text" class="form-control"  value="{{$ticket->nombre}}"  autocomplete="off" autofocus="on" autofocus required="" pattern="[a-z A-Z ñÑ ÁÉÍÓÚáéíóú ]+">    
+    <input id="NOMBRE" name="NOMBRE" type="text" class="form-control" value="{{$ticket->nombre}}" autocomplete="off" autofocus="on" autofocus required=""  pattern="[a-z A-Z ñÑ ÁÉÍÓÚáéíóú ]+"  onkeyup="DobleEspacio(this, event);" value="{{$ticket->ubicacion}}" required>
+  </div> onkeypress="return sololetras(event)">
   </div>
 
   <div class="mb-3">
     <label for="" class="form-label">TELEFONO</label>
-    <input id="TELEFONO" name="TELEFONO" type="text" class="form-control"  value="{{$ticket->telefono}}" minlength="8" maxlength="13" autocomplete="off" autofocus="on" onkeypress="return isNumberKey(event)" autofocus required="" pattern="[0-9]+">    
+    <input id="TELEFONO" name="TELEFONO" type="text" class="form-control" value="{{$ticket->telefono}}" minlength="8" maxlength="13" autocomplete="off" autofocus="on" onkeypress="return isNumberKey(event)" autofocus required="" pattern="[0-9]+">
   </div>
 
 
   <div class="mb-3">
     <label for="" class="form-label">CORREO_ELECTRONICO</label>
-    <input id="CORREO_ELECTRONICO" name="CORREO_ELECTRONICO" type="email" class="form-control"  value="{{$ticket->correo_electronico}}" required>    
+    <input id="CORREO_ELECTRONICO" name="CORREO_ELECTRONICO" type="email" class="form-control" value="{{$ticket->correo_electronico}}" required>
   </div>
 
   <div class="mb-3">
     <label for="" class="form-label">TEMA</label>
-    <input id="TEMA" name="TEMA" type="text" class="form-control"  value="{{$ticket->tema}}" required>    
+    <input id="TEMA" name="TEMA" type="text" class="form-control" onkeyup="DobleEspacio(this, event);" value="{{$ticket->tema}}" required>
   </div>
 
   <div class="mb-3">
     <label for="" class="form-label">DESCRIPCION</label>
-    <input id="DESCRIPCION" name="DESCRIPCION" type="text" class="form-control"  value="{{$ticket->descripcion}}" required>    
+    <input id="DESCRIPCION" name="DESCRIPCION" type="text" class="form-control" onkeyup="DobleEspacio(this, event);" value="{{$ticket->descripcion}}" required>
   </div>
 
 
   <div class="mb-3">
     <label for="" class="form-label">UBICACION</label>
-    <input id="UBICACION" name="UBICACION" type="text" class="form-control"  value="{{$ticket->ubicacion}}" required>    
+    <input id="UBICACION" name="UBICACION" type="text" class="form-control" onkeyup="DobleEspacio(this, event);" value="{{$ticket->ubicacion}}" required>
   </div>
 
   <div>
     <label for="country">COD_ESTADO</label>
-    <select class="form-control" id="COD_ESTADO" name="COD_ESTADO" value="{{$ticket->cod_estado}}" >
-      <option value="{{$ticket->cod_estado}}" >Selecciona nuestros servicios</option>
+    <select class="form-control" id="COD_ESTADO" name="COD_ESTADO" value="{{$ticket->cod_estado}}">
+      <option value="{{$ticket->cod_estado}}">Selecciona nuestros servicios</option>
       <option value="1">NUEVO</option>
       <option value="2">EN PROCESO</option>
       <option value="3">FINALIZADO</option>
@@ -163,11 +205,11 @@ e.value = (valor.indexOf(".") >= 0) ? (valor.substr(0, valor.indexOf(".")) + val
   </div>
   <div class="mb-3">
     <label for="" class="form-label">nombre_empleado</label>
-    <input id="NOMBRE_EMPLEADO" name="NOMBRE_EMPLEADO" type="text" class="form-control"  value="{{$ticket->nombre_empleado}}" required>    
+    <input id="NOMBRE_EMPLEADO" name="NOMBRE_EMPLEADO" type="text" class="form-control" value="{{$ticket->nombre_empleado}}" required>
   </div>
 
   <a href="/ticket" class="btn btn-secondary" tabindex="5">Cancelar</a>
   <button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
 </form>
-    
+
 @stop
