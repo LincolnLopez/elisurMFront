e83e8f3a05adecfd27ebfef95acf56d3b4a3be08
LincolnLopez <93690898+LincolnLopez@users.noreply.github.com>
2022-12-12 17:50:25 -0600

    Validaciones articulos

--- resources/views/articulos/create.blade.php
+++ resources/views/articulos/create.blade.php
@@ -25,219 +25,141 @@
 
 
 
-    < <script>
-        * FUNCION PARA RESTRINGIR EL ESPACIO *
-            **
-            **
-            **
-            **
-            **
-            ** ** ** ** ** ** ** ** ** ** ** ** ** ** * /
-    </script>
+<script>
+    /*=============================================
+     VALIDACION QUE SOLO PERMITA LETRAS Y NUMEROS             
+    =============================================*/
 
-    <script>
-        /*=============================================
-                     VALIDACION QUE SOLO PERMITA LETRAS Y NUMEROS             
-                    =============================================*/
+    function letrasynumeros(e) {
 
-        function letrasynumeros(e) {
+        key = e.keyCode || e.wich;
 
-            key = e.keyCode || e.wich;
+        teclado = String.fromCharCode(key).toUpperCase();
 
-            teclado = String.fromCharCode(key).toUpperCase();
+        letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZÑ1234567890 ";
 
-            letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZÑ1234567890 ";
+        especiales = "8-37-38-46-164";
 
-            especiales = "8-37-38-46-164";
+        teclado_especial = false;
 
-            teclado_especial = false;
+        for (var i in especiales) {
 
-            for (var i in especiales) {
-
-                if (key == especiales[i]) {
-                    teclado_especial = true;
-                    break;
-                }
-            }
-
-            if (letras.indexOf(teclado) == -1 && !teclado_especial) {
-                return false;
-            }
-
-        }
-
-        /*=====  End of Section comment block  ======*/
-
-
-        /*==============================================
-        =     VALIDACION SOLO LETRAS            =
-        ==============================================*/
-        function sololetras(e) {
-
-            key = e.keyCode || e.wich;
-
-            teclado = String.fromCharCode(key).toUpperCase();
-
-            letras = " ABCDEFGHIJKLMNOPQRSTUVWXYZÑ";
-
-            especiales = "8-37-38-46-164";
-
-            teclado_especial = false;
-
-            for (var i in especiales) {
-
-                if (key == especiales[i]) {
-                    teclado_especial = true;
-                    break;
-                }
-            }
-
-            if (letras.indexOf(teclado) == -1 && !teclado_especial) {
-                return false;
-            }
-
-        }
-
-
-
-
-        /*==============================================
-        =        VALIDACION SOLO NUMEROS           =
-        ==============================================*/
-        function solonumeros(e) {
-
-            key = e.keyCode || e.wich;
-
-            teclado = String.fromCharCode(key).toUpperCase();
-
-            letras = "1234567890";
-
-            especiales = "8-37-38-46-164";
-
-            teclado_especial = false;
-
-            for (var i in especiales) {
-
-                if (key == especiales[i]) {
-                    teclado_especial = true;
-                    break;
-                }
-            }
-
-            if (letras.indexOf(teclado) == -1 && !teclado_especial) {
-                return false;
-            }
-
-        }
-
-        function Espacio(campo, event) {
-            CadenaaReemplazar = " ";
-            CadenaReemplazo = "";
-            CadenaTexto = campo.value;
-            CadenaTextoNueva = CadenaTexto.split(CadenaaReemplazar).join(CadenaReemplazo);
-            campo.value = CadenaTextoNueva;
-        }
-
-
-
-
-        function limitDecimalPlaces(e, count) {
-            if (e.target.value.indexOf('.') == -1) {
-                return;
-            }
-            if ((e.target.value.length - e.target.value.indexOf('.')) > count) {
-                e.target.value = parseFloat(e.target.value).toFixed(count);
+            if (key == especiales[i]) {
+                teclado_especial = true;
+                break;
             }
         }
 
-        function isNumberKey(evt) {
-            var charCode = (evt.which) ? evt.which : evt.keyCode;
-            if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
-                return false;
-
-            return true;
-        }
-    </script>
-
-    <script>
-        function DobleEspacio(campo, event) {
-
-            CadenaaReemplazar = "  ";
-            CadenaReemplazo = " ";
-            CadenaTexto = campo.value;
-            CadenaTextoNueva = CadenaTexto.split(CadenaaReemplazar).join(CadenaReemplazo);
-            campo.value = CadenaTextoNueva;
-
-        }
-    </script>
-
-    <script>
-        function soloLetras(e) {
-            key = e.keyCode || e.which;
-            tecla = String.fromCharCode(key).toLowerCase();
-            letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
-            especiales = "8-37-39-46";
-
-            tecla_especial = false
-            for (var i in especiales) {
-                if (key == especiales[i]) {
-                    tecla_especial = true;
-                    break;
-                }
-            }
-
-            if (letras.indexOf(tecla) == -1 && !tecla_especial) {
-                return false;
-            }
-        }
-    </script>
-
-
-
-
-
-    <script>
-        function nospaces2() {
-            orig = document.CORREO.value;
-            nuev = orig.split(' ');
-            nuev = nuev.join('');
-            document.form.CORREO.value = nuev;
-            if (nuev = orig.split(' ').length >= 2);
+        if (letras.indexOf(teclado) == -1 && !teclado_especial) {
+            return false;
         }
 
-        function validar() {
-            var correo, expresion;
-            correo = document.getElementById("correo").value;
-            expresion = /\w+@\w+\.+[a-z]/;
+    }
 
-            if (correo.length > 80) {
-                alert("El campo correo excede su capacidad de caracteres");
-            } else if (!expresion.test(correo)) {
-                alert('El correo no es valido');
-                return false;
+    /*=====  End of Section comment block  ======*/
+
+
+    /*==============================================
+    =     VALIDACION SOLO LETRAS            =
+    ==============================================*/
+    function sololetras(e) {
+
+        key = e.keyCode || e.wich;
+
+        teclado = String.fromCharCode(key).toUpperCase();
+
+        letras = " ABCDEFGHIJKLMNOPQRSTUVWXYZÑ";
+
+        especiales = "8-37-38-46-164";
+
+        teclado_especial = false;
+
+        for (var i in especiales) {
+
+            if (key == especiales[i]) {
+                teclado_especial = true;
+                break;
             }
         }
 
-        /*
-        function validar() {
-        if (/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3,4})+$/.test('correo')){
-        alert("La dirección de email " + 'correo' + " es correcta.");
-        }else {
-        alert("La dirección de email es incorrecta.");
+        if (letras.indexOf(teclado) == -1 && !teclado_especial) {
+            return false;
         }
-        }*/
-    </script>
+
+    }
 
 
-    <script>
-        function unspaces() {
-            orig = document.form.NOMBRE_EMPLEADO_.value;
-            nuev = orig.split('  ');
-            nuev = nuev.join(' ');
-            document.form.NOMBRE_EMPLEADO_.value = nuev;
-            if (nuev = orig.split(' ').length >= 2);
+
+
+    /*==============================================
+    =        VALIDACION SOLO NUMEROS           =
+    ==============================================*/
+    function solonumeros(e) {
+
+        key = e.keyCode || e.wich;
+
+        teclado = String.fromCharCode(key).toUpperCase();
+
+        letras = "1234567890";
+
+        especiales = "8-37-38-46-164";
+
+        teclado_especial = false;
+
+        for (var i in especiales) {
+
+            if (key == especiales[i]) {
+                teclado_especial = true;
+                break;
+            }
         }
-    </script>
+
+        if (letras.indexOf(teclado) == -1 && !teclado_especial) {
+            return false;
+        }
+
+    }
+
+    function Espacio(campo, event) {
+        CadenaaReemplazar = " ";
+        CadenaReemplazo = "";
+        CadenaTexto = campo.value;
+        CadenaTextoNueva = CadenaTexto.split(CadenaaReemplazar).join(CadenaReemplazo);
+        campo.value = CadenaTextoNueva;
+    }
+
+
+
+
+    function limitDecimalPlaces(e, count) {
+        if (e.target.value.indexOf('.') == -1) {
+            return;
+        }
+        if ((e.target.value.length - e.target.value.indexOf('.')) > count) {
+            e.target.value = parseFloat(e.target.value).toFixed(count);
+        }
+    }
+
+    function isNumberKey(evt) {
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+
+        return true;
+    }
+</script>
+
+<script>
+    function DobleEspacio(campo, event) {
+
+        CadenaaReemplazar = "  ";
+        CadenaReemplazo = " ";
+        CadenaTexto = campo.value;
+        CadenaTextoNueva = CadenaTexto.split(CadenaaReemplazar).join(CadenaReemplazo);
+        campo.value = CadenaTextoNueva;
+
+    }
+</script>
 
 
 
@@ -249,7 +171,7 @@
 @section('content')
     <form action="/articulos" method="POST" onsubmit="return validar();" autocomplete="off">
         @csrf
-
+ 
         <div class="container-fluid">
 
             <!-- Main row -->
@@ -271,9 +193,9 @@
                                     <!--Que se ubique el cursos en el textbox autofocus="on"-->
 
                                     <input id="NOMBRE_ARTICULO" name="NOMBRE_ARTICULO" type="text" class="form-control"
-                                        tabindex="1" autocomplete="off" autofocus="on"
-                                        onkeyup="DobleEspacio(this, event);" onkeypress="return sololetras(event)"
-                                        placeholder="Ingrese"autofocus required>
+                                    tabindex="1" autocomplete="off" autofocus="on"
+                                    onkeyup="DobleEspacio(this, event);" onkeypress="return letrasynumeros(event)"
+                                    placeholder="Ingrese Nombre"autofocus required>
 
                                 </div>
                                 <!--campo requerido required    Que no se pueda pegar onPaste="return false;"-->
@@ -281,10 +203,10 @@
 
                                 <div class="mb-3">
                                     <label for="" class="form-label">DESCRIPCION ARTICULO</label>
-                                    <input id="DESCRIPCION_ARTICULO" name="DESCRIPCION_ARTICULO" type="text"
-                                        class="form-control" tabindex="1" autocomplete="off" autofocus="on"
-                                        onkeypress="return letrasynumeros(event)" onkeyup="DobleEspacio(this, event);"
-                                        placeholder="Ingrese una breve descripción del producto" required>
+                                    <input id="DESCRIPCION_ARTICULO" name="DESCRIPCION_ARTICULO" type="text" class="form-control"
+                                    tabindex="1" autocomplete="off" autofocus="on"
+                                    onkeyup="DobleEspacio(this, event);" onkeypress="return letrasynumeros(event)"
+                                    placeholder="Ingrese la descripción"autofocus required>
                                 </div>
 
 
--- resources/views/articulos/edit.blade.php
+++ resources/views/articulos/edit.blade.php
@@ -24,70 +24,140 @@
 
             <!---validació de numeros y 2 decimales--->
             <script>
-              function DobleEspacio(campo, event) {
+    /*=============================================
+     VALIDACION QUE SOLO PERMITA LETRAS Y NUMEROS             
+    =============================================*/
 
-                CadenaaReemplazar = "  ";
-                CadenaReemplazo = " ";
-                CadenaTexto = campo.value;
-                CadenaTextoNueva = CadenaTexto.split(CadenaaReemplazar).join(CadenaReemplazo);
-                campo.value = CadenaTextoNueva;
+    function letrasynumeros(e) {
 
-              }
+        key = e.keyCode || e.wich;
 
+        teclado = String.fromCharCode(key).toUpperCase();
 
+        letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZÑ1234567890 ";
 
+        especiales = "8-37-38-46-164";
 
-              function letrasynumeros(e) {
+        teclado_especial = false;
 
-                key = e.keyCode || e.wich;
+        for (var i in especiales) {
 
-                teclado = String.fromCharCode(key).toUpperCase();
+            if (key == especiales[i]) {
+                teclado_especial = true;
+                break;
+            }
+        }
 
-                letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZÑ1234567890 ";
+        if (letras.indexOf(teclado) == -1 && !teclado_especial) {
+            return false;
+        }
 
-                especiales = "8-37-38-46-164";
+    }
 
-                teclado_especial = false;
-
-                for (var i in especiales) {
-
-                  if (key == especiales[i]) {
-                    teclado_especial = true;
-                    break;
-                  }
-                }
-
-                if (letras.indexOf(teclado) == -1 && !teclado_especial) {
-                  return false;
-                }
-
-              }
+    /*=====  End of Section comment block  ======*/
 
 
+    /*==============================================
+    =     VALIDACION SOLO LETRAS            =
+    ==============================================*/
+    function sololetras(e) {
+
+        key = e.keyCode || e.wich;
+
+        teclado = String.fromCharCode(key).toUpperCase();
+
+        letras = " ABCDEFGHIJKLMNOPQRSTUVWXYZÑ";
+
+        especiales = "8-37-38-46-164";
+
+        teclado_especial = false;
+
+        for (var i in especiales) {
+
+            if (key == especiales[i]) {
+                teclado_especial = true;
+                break;
+            }
+        }
+
+        if (letras.indexOf(teclado) == -1 && !teclado_especial) {
+            return false;
+        }
+
+    }
 
 
 
 
+    /*==============================================
+    =        VALIDACION SOLO NUMEROS           =
+    ==============================================*/
+    function solonumeros(e) {
+
+        key = e.keyCode || e.wich;
+
+        teclado = String.fromCharCode(key).toUpperCase();
+
+        letras = "1234567890";
+
+        especiales = "8-37-38-46-164";
+
+        teclado_especial = false;
+
+        for (var i in especiales) {
+
+            if (key == especiales[i]) {
+                teclado_especial = true;
+                break;
+            }
+        }
+
+        if (letras.indexOf(teclado) == -1 && !teclado_especial) {
+            return false;
+        }
+
+    }
+
+    function Espacio(campo, event) {
+        CadenaaReemplazar = " ";
+        CadenaReemplazo = "";
+        CadenaTexto = campo.value;
+        CadenaTextoNueva = CadenaTexto.split(CadenaaReemplazar).join(CadenaReemplazo);
+        campo.value = CadenaTextoNueva;
+    }
 
 
 
-              function limitDecimalPlaces(e, count) {
-                if (e.target.value.indexOf('.') == -1) {
-                  return;
-                }
-                if ((e.target.value.length - e.target.value.indexOf('.')) > count) {
-                  e.target.value = parseFloat(e.target.value).toFixed(count);
-                }
-              }
 
-              function isNumberKey(evt) {
-                var charCode = (evt.which) ? evt.which : evt.keyCode;
-                if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
-                  return false;
+    function limitDecimalPlaces(e, count) {
+        if (e.target.value.indexOf('.') == -1) {
+            return;
+        }
+        if ((e.target.value.length - e.target.value.indexOf('.')) > count) {
+            e.target.value = parseFloat(e.target.value).toFixed(count);
+        }
+    }
 
-                return true;
-              }
-            </script>
+    function isNumberKey(evt) {
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+
+        return true;
+    }
+</script>
+
+<script>
+    function DobleEspacio(campo, event) {
+
+        CadenaaReemplazar = "  ";
+        CadenaReemplazo = " ";
+        CadenaTexto = campo.value;
+        CadenaTextoNueva = CadenaTexto.split(CadenaaReemplazar).join(CadenaReemplazo);
+        campo.value = CadenaTextoNueva;
+
+    }
+</script>
 
 
             <div class="mb-3">
@@ -99,24 +169,34 @@
 
             <div class="mb-3">
               <label for="" class="form-label">NOMBRE DE ARTICULO</label>
-              <input id="NOMBRE_ARTICULO" name="NOMBRE_ARTICULO" type="text" class="form-control" onkeyup="DobleEspacio(this, event);" onkeypress="return letrasynumeros(event)" value="{{$articulo->nombre_articulo}}" required autofocus>
+              <input id="NOMBRE_ARTICULO" name="NOMBRE_ARTICULO" type="text" class="form-control"
+              tabindex="1" autocomplete="off" autofocus="on"
+              onkeyup="DobleEspacio(this, event);" onkeypress="return letrasynumeros(event)"
+              placeholder="Ingrese la descripción"autofocus required value="{{$articulo->nombre_articulo}}">
             </div>
 
 
             <div class="mb-3">
               <label for="" class="form-label">DESCRIPCION ARTICULO</label>
-              <input id="DESCRIPCION_ARTICULO" name="DESCRIPCION_ARTICULO" type="text" class="form-control" onkeyup="DobleEspacio(this, event);" value="{{$articulo->descripcion_articulo}}" required autofocus>
+              <input id="DESCRIPCION_ARTICULO" name="DESCRIPCION_ARTICULO" type="text" class="form-control"
+              tabindex="1" autocomplete="off" autofocus="on"
+              onkeyup="DobleEspacio(this, event);" onkeypress="return letrasynumeros(event)"
+              placeholder="Ingrese la descripción"autofocus required onkeyup="DobleEspacio(this, event);" value="{{$articulo->descripcion_articulo}}">
             </div>
 
             <div class="mb-3">
               <label for="" class="form-label">PRECIO ARTICULO</label>
-              <input id="PRECIO_ARTICULO" name="PRECIO_ARTICULO" type="text" class="form-control" value="{{$articulo->precio_articulo}}" autofocus required oninput="limitDecimalPlaces(event, 2)" onkeypress="return isNumberKey(event)" autofocus required>
+              <input id="PRECIO_ARTICULO" name="PRECIO_ARTICULO" type="text" class="form-control"
+              tabindex="1" autofocus required oninput="limitDecimalPlaces(event, 2)"
+              onkeypress="return isNumberKey(event)" autofocus required minlength="2" maxlength="10" value="{{$articulo->precio_articulo}}" autofocus required oninput="limitDecimalPlaces(event, 2)" onkeypress="return isNumberKey(event)">
             </div>
 
             <label for="country">CODIGO CATEGORIA</label>
             <select class="form-control" id="COD_CATEGORIA" name="COD_CATEGORIA" value="{{$articulo->cod_categoria}}" required autofocus>
               <option value="1">Tapiceria</option>
-              <option value="21">Construcción</option>
+               <option value="2">Albañileria</option>
+              <option value="3">Carpinteria</option>
+              <option value="4">Redes</option>
 
             </select>
 
